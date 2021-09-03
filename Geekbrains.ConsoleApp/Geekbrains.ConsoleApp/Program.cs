1cf3ce70b21fe2b0a0e824a75fe9f0291f876559
User <User@DESKTOP-RLOLFP4>
2021-09-03 18:43:53 +0300

    lesson4

--- Geekbrains.ConsoleApp/Geekbrains.ConsoleApp/Program.cs
+++ Geekbrains.ConsoleApp/Geekbrains.ConsoleApp/Program.cs
@@ -6,12 +6,37 @@ namespace Geekbrains.ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			var lesson3 = new Lesson3();
-			//lesson3.LogArray();
-			//lesson3.LogArrayReverse();
-			lesson3.GoodArray();
+			var lesson4 = new lesson4();
+			Console.WriteLine(lesson4.GetFullName("a", "b", "c"));
+			Console.WriteLine(lesson4.Sum("1 2 3 4"));
+			var yeartype = lesson4.GetYearType(9);
+			YearTypeToString(yeartype);
+			 
 			Console.ReadKey();
 		}
+		static void YearTypeToString(YearType yearType)
+		{
+			switch (yearType)
+			{
+				case YearType.Winter:
+					Console.WriteLine("Зима");
+					break;
+				case YearType.Spring:
+					Console.WriteLine("Весна");
+					break;
+				case YearType.Summer:
+					Console.WriteLine("Лето");
+					break;
+				case YearType.Autumn:
+					Console.WriteLine("Осень");
+					break;
+				default:
+				Console.WriteLine("Ошибка: введите число от 1 до 12");
+					break;
+
+			}
+		}
+
 	}
 
 }
